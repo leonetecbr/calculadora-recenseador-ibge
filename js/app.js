0da2918d65f6078fb362d8383f2b255115859200
Leone Oliveira <falecom@leone.tec.br>
2022-08-27 19:55:09 -0300

    Muda um pouco o visual

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
 let taxes = {
     1: {
         urban: {
